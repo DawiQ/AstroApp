da3e79490ca2a943bb76575d9ee05c6ef9a19e1e
Przemysław Prencel <hartag96@gmail.com>
2019-10-31 09:42:48 +0100

    heroku key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,10 @@ class JsonWebToken
  class << self
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
-     JWT.encode(payload, Rails.application.secrets.secret_key_base)
+     @key = "a80c492beecb0159f031575e94ec4d80348b4d64479eae5e5d67804db6dce9accab874a26154ae2d4e31136729c1fde10458c5da04524fe166b01d1e3b4da21e"
+     Rails.logger.info(payload)
+     Rails.logger.info(@key)
+     JWT.encode(payload, @key)
    end
 
    def decode(token)
